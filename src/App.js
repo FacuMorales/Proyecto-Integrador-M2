0c295164a8a9e07be7126adc818d8cf549df6d89
FacuMorales <facu44515119@gmail.com>
2024-02-15 16:54:00 -0300

    Proyector terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cards from './components/Cards/Cards.jsx';
+import Nav from "./components/Nav/Nav.jsx";
+import About from "./components/About/About.jsx";
+import Detail from "./components/Detail/Detail.jsx";
+import Form from "./components/Form/Form.jsx";
+import Favorites from "./components/Favorites/Favorites.jsx";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {Routes, Route, useLocation, useNavigate} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [characters, setCharacters] = useState([]);
+   const [access, setAccess] = useState(false);
+   const EMAIL = "facu44515119@gmail.com";
+   const PASSWORD = "facu14";
+   const location = useLocation();
+   const navigate = useNavigate();
+
+   function login (userData){
+      if (userData.password === PASSWORD && userData.email === EMAIL) {
+         setAccess(true);
+         navigate('/home');
+      }
+   }
+
+   useEffect(() => {
+      !access && navigate('/');
+   }, [access]);
+
+   function onSearch(id) {
+      axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+         ({ data }) => {
+            if (data.name) {
+               setCharacters([...characters, data]);
+            } else {
+               window.alert('¡No hay personajes con este ID!');
+            }
+         }
+      );
+   }
+
+   function onClose(id){
+      const newId = parseInt(id, 10);
+      const newArray = characters.filter((pj)=>{
+         return pj.id !== newId;
+      });
+      setCharacters(newArray);
+   }
+
+   function navRender () {
+      if (location.pathname !== '/') return <Nav onSearch={onSearch}/>;  
+      else return <></>;
+   }
+
+   return (
+      <div className="App">
+         {navRender()}
+         <Routes>
+            <Route path="/" element={<Form login={login}/>} />
+            <Route path="/home" element={<Cards characters={characters} onClose={onClose}/>} />
+            <Route path="/about" element={<About />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/favorites" element={<Favorites onClose={onClose}/>} />
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/About/About.jsx
@@ -0,0 +1,30 @@
+import style from "./About.module.css";
+import imgAbout from "./rick-and-morty.gif";
+
+function About(){
+    return(
+        <div className={style.container}>
+            <h1>Proyecto Integrador: App Rick & Morty</h1>
+            <h3>Aplicación desarrollada con React y React-Redux</h3>
+            <h4>Desarrollador: Facundo Morales</h4>
+            <p>
+                Esta es mi primer aplicación en React.<br/>
+                En ella apliqué todas las tecnologias y habilidades aprendidas hasta el momento en el bootcamp.<br/>
+                Podemos ver cosas como la modularización de componentes, estados, peticiones, buscador, filtrado, ordenamiento, estilos, etc.<br/>
+                ¡Te invito a que la pruebes!
+            </p>
+            <h4>Un poco sobre mi:</h4>
+            <p>
+                Me llamo Facundo Morales, tengo 21 años y soy de Jujuy, Argentina, una provincia ubicada al norte del país.<br/>
+                Estudié durante 2 años en la facultad la carrera de Analista Programador Universitario, pero no terminó de agradarme el formato y tecnologias enseñadas.<br/>
+                Luego leí y descubrí SoyHenry, me interesó bastante el como planteaban el bootcamp, asi que me adentré y averigüe que necesitaba para acceder.<br/>
+                Creé mi cuenta y empecé el PrepCourse, el cual no me costó demasiado debido a mis conocimientos previos gracias a lo aprendido en los 2 años de carrera.<br/>
+                Finalmente rendí el HenryChallenge y logré aprobarlo a la primera, y fue asi como empecé en este grandioso camino.
+            </p>
+            <img src={imgAbout} alt="" className={style.img} />
+            <h4>¡Gracias por ver mi aplicación!</h4>
+        </div>
+    );
+}
+
+export default About;
